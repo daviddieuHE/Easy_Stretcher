3f395b0bfe934a7bd5d13afd40b977c0ae66a492
HE201931 <71380301+HE201931@users.noreply.github.com>
2022-03-12 21:39:40 +0100

    Update app.tsx

--- Easy_Stretcher/app.tsx
+++ Easy_Stretcher/app.tsx
@@ -3,6 +3,7 @@ declare var require: any
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+export class Hello extends React.Component {
     render() {
         return (
             <h1>Welcome to React!!</h1>
