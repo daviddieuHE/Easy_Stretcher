250d221b0e63d2675c5561e5b2682e0eaedb2887
HE201931 <71380301+HE201931@users.noreply.github.com>
2022-03-14 17:54:34 +0100

    Update server.js

--- Easy_Stretcher/server.js
+++ Easy_Stretcher/server.js
@@ -13,3 +13,28 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function () {
     console.log('listening');
 });
+
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+
+    host: "devweb.sytes.net",
+
+    user: "mabite",
+
+    password: "METTEZ LE MDP QUE JAI ENVOYE",
+
+    database: "hopital"
+
+});
+
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+    db.query("select * from tb_status", function (err, result) {
+        //if (err) throw err;
+        console.log(err);
+        console.log(result);
+    });
+});
