6e371beb2a5374aa2668bf7b7f755d8887f14d8d
daviddieuHE <92298816+daviddieuHE@users.noreply.github.com>
2022-03-31 17:04:41 +0200

    Delete mon-app directory

--- mon-app/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const db = require("../src/server");
-const router = express.Router();
-
-// @route GET && POST - /posts/
-router.get();
-
-module.exports = router;
