077d9856416ef4398faf4c1da31f5a38fcc6673d
FirminMoyen <firminmoyen@gmail.com>
2022-05-16 12:34:05 +0200

    testing

--- cypress/integration/brancardier_test.js
+++ cypress/integration/brancardier_test.js
@@ -1,15 +1,46 @@
-describe('brancardier', () => {
-    it('brancardier can do everything', () => {
-        //check login
-        cy.visit('http://localhost:3001/');
-        cy.findByRole('button', {  name: /login/i}).click();
-        cy.findByText(/bad user or password/i);
-        //login
-        cy.findByRole('textbox', {  name: /email/i}).type('bran@admin.com');
-        cy.findByLabelText(/password/i).type('123');
-        cy.findByRole('button', {  name: /login/i}).click();
+describe('Brancardier', () => {
+    it('Brancardier page can render', () => {
+        cy.visit('/');
+        cy.get('#email').type('bran@admin.com');
+        cy.get('#password').type('123');
+        cy.get('[type="submit"]').click();
 
-        //logout
-        cy.findByRole('button', {  name: /logout/i}).click();
+        cy.get('[data-testid="branDiv"]').should("exist");
+        cy.get('.navbar').should("exist");
+        cy.findByRole('heading', {  name: /demande de transport/i}).should("exist");
+        cy.findByRole('heading', {  name: /transport en cours/i}).should("exist");
+        cy.findByRole('heading', {  name: /liste de travail/i}).should("exist");
+    });
+
+    it('Brancardier can change date', () => {
+        cy.get('[data-testid="dayPicker"]').should("exist");
+        cy.get('[data-testid="dayPicker"]').click();
+        cy.get('[data-testid="dayPicker"]').type('2022-05-17');
+    });
+
+    it('worklist can render', () => {
+        cy.get('#Bacq').should("exist");
+    });
+
+    it('brancardier can accept a patient', () => {
+        cy.get('#Cross > .patient-action > :nth-child(2) > :nth-child(1) > .action > .action-button').click();
+        cy.get('#Vandenberg > .patient-action > :nth-child(2) > :nth-child(1) > .action > .action-button').click();
+        cy.get('#Bacq > .patient-action > :nth-child(2) > :nth-child(1) > .action > .action-button').click();
+
+        cy.get('#Cross > .patient-action > :nth-child(2) > .action > .action-button').should("exist");
+        cy.get('#Vandenberg > .patient-action > :nth-child(2) > .action > .action-button').should("exist");
+        cy.get('#Bacq > .patient-action > :nth-child(2) > .action > .action-button').should("exist");
+    });
+
+    it('brancardier can dump a patient', () => {
+        cy.get('#Cross > .patient-action > :nth-child(2) > .action > .action-button').click();
+        cy.get('#Vandenberg > .patient-action > :nth-child(2) > .action > .action-button').click();
+        cy.get('#Bacq > .patient-action > :nth-child(2) > .action > .action-button').click();
+    });
+
+    it('Brancardier can logout', () => {
+        cy.get('#logout').click();
+
+        cy.get('[data-testid="loginDiv"]').should("exist");
     });
 });
